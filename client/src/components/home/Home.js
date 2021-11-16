3668ab5a2f59c6ed2d769a8ca336e680c4d1b66a
Jon-dough88 <guy.goodman.rater@gmail.com>
2021-11-16 23:52:19 +0200

    Added a cosntructor to the Home component.

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {CSSTransition, TransitionGroup } from 'react-transition-group';
-
+import './Home.css';
 
 import Navmenu from '../navbar/Navmenu';
 import MainPage from '../main/MainPage';
@@ -10,21 +10,29 @@ import ProjectPage from '../projects/Projects';
 import Contact from '../contact/Contact';
 
 class Home extends Component {
+    constructor(){
+        super()
+        this.state={}
+    }
 
     render() {
 
+        const location = this.props
         return (
             <Router>
                 <div>
              
                     <Navmenu />
-
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/projects" element={<ProjectPage />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
+                <TransitionGroup>   
+                    <CSSTransition timeout={300} classNames="fade">
+                        <Routes>
+                            <Route exact path="/" element={<MainPage />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/projects" element={<ProjectPage />} />
+                            <Route path="/contact" element={<Contact />} />
+                        </Routes>
+                    </CSSTransition>
+                </TransitionGroup> 
                 </div>
             </Router>
         )
